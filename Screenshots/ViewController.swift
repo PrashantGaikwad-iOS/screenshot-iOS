9c1b0d0a2a8d25834a53eab4faa51483b6ebf1c3
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-06-30 17:22:15 +0530

    screenshot in ios swift

--- Screenshots/ViewController.swift
+++ Screenshots/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var screenshotImageView: UIImageView!
+    @IBOutlet weak var xTextField: UITextField!
+    @IBOutlet weak var yTextField: UITextField!
+    @IBOutlet weak var widthTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func takeScreenshotAction(_ sender: Any) {
+        
+        screenshotImageView.image = self.view.snapshot(of: CGRect(x: Int(self.xTextField.text!)!, y: Int(self.yTextField.text!)!, width: Int(self.widthTextField.text!)!, height: Int(self.heightTextField.text!)!))
+        
+    }
 
 }
 
+
+// UIView screenshot
+extension UIView {
+    
+    /// Create snapshot
+    ///
+    /// - parameter rect: The `CGRect` of the portion of the view to return. If `nil` (or omitted),
+    ///                   return snapshot of the whole view.
+    ///
+    /// - returns: Returns `UIImage` of the specified portion of the view.
+    
+    func snapshot(of rect: CGRect? = nil) -> UIImage? {
+        // snapshot entire view
+        
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0)
+        drawHierarchy(in: bounds, afterScreenUpdates: true)
+        let wholeImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // if no `rect` provided, return image of whole view
+        
+        guard let image = wholeImage, let rect = rect else { return wholeImage }
+        
+        // otherwise, grab specified `rect` of image
+        
+        let scale = image.scale
+        let scaledRect = CGRect(x: rect.origin.x * scale, y: rect.origin.y * scale, width: rect.size.width * scale, height: rect.size.height * scale)
+        guard let cgImage = image.cgImage?.cropping(to: scaledRect) else { return nil }
+        return UIImage(cgImage: cgImage, scale: scale, orientation: .up)
+    }
+    
+}
